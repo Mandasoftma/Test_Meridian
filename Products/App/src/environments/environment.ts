fa78baf9ec7c238013cf6ff3d436622a4e9e770b
Mandasoftma <36303653+Mandasoftma@users.noreply.github.com>
2018-11-14 19:39:19 -0500

    Update environment.ts

--- Products/App/src/environments/environment.ts
+++ Products/App/src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
     firebase:{
-        apiKey: "AIzaSyBINryh8da1gU8UWQ5nuMJvn7CSvKh5V4w",
-        authDomain: "webproducts-2a48d.firebaseapp.com",
-        databaseURL: "https://webproducts-2a48d.firebaseio.com",
-        projectId: "webproducts-2a48d",
-        storageBucket: "webproducts-2a48d.appspot.com",
-        messagingSenderId: "527970662690"
+        apiKey: "xxxxx",
+        authDomain: "webproducts",
+        databaseURL: "https://webproducts",
+        projectId: "webproducts",
+        storageBucket: "webproduct",
+        messagingSenderId: "xxxx"
     }
 };
 
